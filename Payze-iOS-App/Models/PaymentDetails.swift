233e2dd48f5f6954ea9ad7dcf21b0d5f07244e09
Bako <baksoon666@gmail.com>
2021-12-21 18:42:48 +0400

    no message

--- Payze-iOS-App/Models/PaymentDetails.swift
+++ Payze-iOS-App/Models/PaymentDetails.swift
@@ -8,17 +8,35 @@
 import Foundation
 
 struct PaymentDetails {
-    let number: String
-    let cardHolder: String
-    let expirationDate: String
-    let securityNumber: String
-    let transactionId: String
-    let billingAddress: String?
+    var number: String = ""
+    var cardHolder: String = ""
+    var expirationDate: String = ""
+    var securityNumber: String = ""
+    var transactionId: String = ""
+    var billingAddress: String? = ""
+    static let defaultBillingAddress: String = ""
     // TODO delete this
-    static let mock: PaymentDetails = .init(number: "4127421007993973",
+    
+    static let defaultMock: PaymentDetails = .init(number: "4127421007993973",
                                             cardHolder: "SALOME GELASHVILI",
                                             expirationDate: "11/22",
                                             securityNumber: "360",
                                             transactionId: "cdd3e3b5-744c-4cd7-bc2f-b4060a0d382e",
-                                            billingAddress: "")
+                                            billingAddress: "")!
+    
+    init?(number: String?,cardHolder: String?,expirationDate: String?,securityNumber: String?,transactionId: String?,billingAddress: String?) {
+        
+        guard let number = number else { return nil }
+        guard let cardHolder = cardHolder else { return nil }
+        guard let expirationDate = expirationDate else { return nil }
+        guard let securityNumber = securityNumber else { return nil }
+        guard let transactionId = transactionId else { return nil }
+        
+        self.number = number
+        self.cardHolder = cardHolder
+        self.expirationDate = expirationDate
+        self.securityNumber = securityNumber
+        self.transactionId = transactionId
+        self.billingAddress = billingAddress
+    }
 }
--- Payze-iOS-App/ViewController.swift
+++ Payze-iOS-App/ViewController.swift
@@ -12,12 +12,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cardView: UIView!
     
+    @IBOutlet weak var cardNumberTxtField: UITextField!
+    @IBOutlet weak var cardHolderTxtField: UITextField!
+    @IBOutlet weak var cvvTxtField: UITextField!
+    @IBOutlet weak var monthTxtField: UITextField!
+    @IBOutlet weak var yearTxtField: UITextField!
+    @IBOutlet weak var transactionIdTxtField: UITextField!
+    
     private let myService = PaymentService()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .white
         setupCardView()
+        setupTxtFields()
     }
     
     @available(iOS 13.0, *)
@@ -25,10 +32,17 @@ class ViewController: UIViewController {
         return UIHostingController(coder: coder, rootView: CardView())
     }
     
+    @IBAction func makePayment(_ sender: Any) {
+        guard let paymentDetails = PaymentDetails.init(number: cardNumberTxtField.text, cardHolder: cardHolderTxtField.text, expirationDate: makeExpirationDate(s1: monthTxtField.text, s2: yearTxtField.text), securityNumber: cvvTxtField.text, transactionId: transactionIdTxtField.text, billingAddress: "") else { return }
+        
+        myService.startPayment(paymentDetails: paymentDetails) { result in
+            print(result)
+        }
+    }
+    
     @objc func cardViewClicked(sender: UITapGestureRecognizer) {
         print(#function)
-        view.backgroundColor = .green
-        self.myService.startPayment(paymentDetails: PaymentDetails.mock) { result in
+        self.myService.startPayment(paymentDetails: PaymentDetails.defaultMock) { result in
             print(result)
         }
     }
@@ -39,8 +53,36 @@ class ViewController: UIViewController {
         cardView.addGestureRecognizer(gesture)
     }
     
+    private func makeExpirationDate(s1: String?,s2: String?) -> String? {
+        guard let s1 = s1 else { return ""}
+        guard let s2 = s2 else { return ""}
+        return s1 + "/" + s2
+    }
+    
+    private func setupTxtFields() {
+        cardNumberTxtField.delegate = self
+        cardHolderTxtField.delegate = self
+        cvvTxtField.delegate = self
+        monthTxtField.delegate = self
+        yearTxtField.delegate = self
+        transactionIdTxtField.delegate = self
+    }
+    
     deinit {
         print("View Controller deinited")
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField.tag == 0 || textField.tag == 2 {
+            let allowedCharacters = CharacterSet.decimalDigits
+            let characterSet = CharacterSet(charactersIn: string)
+            
+            return allowedCharacters.isSuperset(of: characterSet)
+        } else {
+            return true
+        }
+        
+    }
+}
