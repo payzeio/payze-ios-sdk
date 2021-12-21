9ff98e9176c7704bd26803f672c74c15a410747e
Bako <baksoon666@gmail.com>
2021-12-21 16:27:11 +0400

    changed design

--- Payze-iOS-App/ViewController.swift
+++ Payze-iOS-App/ViewController.swift
@@ -6,23 +6,39 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cardView: UIView!
+    
     private let myService = PaymentService()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        setupCardView()
     }
-
-    @IBAction func sendCardDetails() {
+    
+    @available(iOS 13.0, *)
+    @IBSegueAction func addSwiftUIView(_ coder: NSCoder) -> UIViewController? {
+        return UIHostingController(coder: coder, rootView: CardView())
+    }
+    
+    @objc func cardViewClicked(sender: UITapGestureRecognizer) {
+        print(#function)
         view.backgroundColor = .green
         self.myService.startPayment(paymentDetails: PaymentDetails.mock) { result in
             print(result)
         }
     }
-
+    
+    private func setupCardView() {
+        cardView.isUserInteractionEnabled = true
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(cardViewClicked(sender:)))
+        cardView.addGestureRecognizer(gesture)
+    }
+    
     deinit {
         print("View Controller deinited")
     }
