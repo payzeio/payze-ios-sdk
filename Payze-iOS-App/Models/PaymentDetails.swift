09e485092b1010cc9e8a9f60ff725754ceb06ca4
Bako <baksoon666@gmail.com>
2021-12-22 20:53:40 +0400

    refactored code

--- Payze-iOS-App/Models/PaymentDetails.swift
+++ Payze-iOS-App/Models/PaymentDetails.swift
@@ -15,10 +15,29 @@ struct PaymentDetails {
     let transactionId: String
     let billingAddress: String?
     // TODO delete this
-    static let mock: PaymentDetails = .init(number: "4127421007993973",
+    static let mock: PaymentDetails! = .init(number: "4127421007993973",
                                             cardHolder: "SALOME GELASHVILI",
                                             expirationDate: "11/22",
                                             securityNumber: "360",
                                             transactionId: "cdd3e3b5-744c-4cd7-bc2f-b4060a0d382e",
                                             billingAddress: "")
+
+    
+    init?(number: String?,cardHolder: String?,expirationDate: String?,securityNumber: String?,transactionId: String?, billingAddress: String?) {
+        
+        guard let number = number else { return nil }
+        guard let cardHolder = cardHolder else { return nil }
+        guard let expirationDate = expirationDate else { return nil }
+        guard let securityNumber = securityNumber else { return nil }
+        guard let transactionId = transactionId else { return nil }
+
+        self.number = number
+        self.cardHolder = cardHolder
+        self.expirationDate = expirationDate
+        self.securityNumber = securityNumber
+        self.transactionId = transactionId
+        self.billingAddress = billingAddress
+        
+    }
+    
 }
--- Payze-iOS-App/ViewController.swift
+++ Payze-iOS-App/ViewController.swift
@@ -12,10 +12,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cardView: UIView!
     
+    @IBOutlet private weak var cardNumberTxtField: UITextField!
+    @IBOutlet private weak var cardHolderTxtField: UITextField!
+    @IBOutlet private weak var cvvTxtField: UITextField!
+    @IBOutlet private weak var monthTxtField: UITextField!
+    @IBOutlet private weak var yearTxtField: UITextField!
+    @IBOutlet private weak var transactionIDTxtField: UITextField!
+    
     private let myService = PaymentService()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         view.backgroundColor = .white
         setupCardView()
     }
@@ -24,11 +31,16 @@ class ViewController: UIViewController {
     @IBSegueAction func addSwiftUIView(_ coder: NSCoder) -> UIViewController? {
         return UIHostingController(coder: coder, rootView: CardView())
     }
+    @IBAction func makePayment(_ sender: Any) {
+        guard let paymentDetails = PaymentDetails.init(number: cardNumberTxtField.text, cardHolder: cardHolderTxtField.text, expirationDate: makeExpirationDate(s1: monthTxtField.text, s2: yearTxtField.text), securityNumber: cvvTxtField.text, transactionId: transactionIDTxtField.text, billingAddress: "") else { return }
+        
+        myService.startPayment(paymentDetails: paymentDetails) { result in
+            print(result)
+        }
+    }
     
     @objc func cardViewClicked(sender: UITapGestureRecognizer) {
-        print(#function)
-        view.backgroundColor = .green
-        self.myService.startPayment(paymentDetails: PaymentDetails.mock) { result in
+        myService.startPayment(paymentDetails: PaymentDetails.mock) { result in
             print(result)
         }
     }
@@ -37,6 +49,13 @@ class ViewController: UIViewController {
         cardView.isUserInteractionEnabled = true
         let gesture = UITapGestureRecognizer(target: self, action: #selector(cardViewClicked(sender:)))
         cardView.addGestureRecognizer(gesture)
+    }
+    
+    private func makeExpirationDate(s1: String?, s2: String?) -> String? {
+        guard let s1 = s1 else { return ""}
+        guard let s2 = s2 else { return ""}
+        return s1 + "/" + s2
+        
     }
     
     deinit {
